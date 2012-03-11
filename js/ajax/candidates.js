139e64bd343bd06755683d421ba422958b0fa7de
Taylor Fairbank <jtfairbank@gmail.com>
2012-03-10 20:10:28 -0600

    Refactored CSS, added candidates page.  Resolves #5.

---
+++ js/ajax/candidates.js
@@ -0,0 +1,106 @@
+[
+    {
+        "name":  "Rajiv Dwivedi",
+        "pos":   "President",
+        "dept":  "",
+        "img":   ".jpg",
+        "email": "rxd099120@utdallas.edu",
+        "bio":   "Raj(iv) Dwivedi is running for President. Majoring in Chemistry, he will be classified a junior starting this fall.  He is currently involved with the Chi Phi fraternity and the Orientation Team Mentors program. He also serves as a student worker at the Student Union at UT Dallas. He wishes to help expand the various services at UT Dallas including the Comet discount program, transportation, food options, internet etc. He has served on senate for the term 2010-2011 as the Legislative Affairs Chair serving on multiple university wide committees."
+    },
+    {
+        "name":  "Nate Fairbank",
+        "pos":   "Vice President",
+        "dept":  "",
+        "img":   "mother_fucking_badass.jpg",
+        "email": "nathaniel.fairbank@utdallas.edu",
+        "bio":   "Nate Fairbank is running for Vice President of Student Government. He has not declared a major, but is thinking about Applied Mathematics. Nate is involved in club Ultimate Frisbee, Jujitsu, Spanish Club, and the McDermott Scholars Program. Nate is running for Vice President to ensure that students get a say in how UT Dallas grows in the years to come. He is currently serving as a Freshman Senator on the Residential Affairs Committee and the Sustainability Subcommittee."
+    },
+    {
+        "name":  "Jakcy Lam",
+        "pos":   "Senior Senator",
+        "dept":  "School of Computer Science",
+        "img":   "lam.jpg",
+        "email": "kxl111830@utdallas.edu",
+        "bio":   "Jacky Lam is running for a senior senator position representing School of Engineering & Computer Science . He is majoring in Computer Science, minor in Business Administration and is involved in Japanese Student Association, Chinese Student Association, Vietnamese Student Association, Korean Undergraduate Student Association. Jacky Lam is running for Senate in order to ensure students' opinions are heard and respected. He is currently serving as a Junior Senator on the Graduate and International Committee."
+    },
+    {
+        "name":  "Shawn Orliss",
+        "pos":   "Senior Senator",
+        "dept":  "School of Economic, Political, and Policy Sciences",
+        "img":   "orliss.jpg",
+        "email": "sbo092000@utdallas.edu",
+        "bio":   "Shawn Orliss is running for a Senior Senator position representing the School of Economic, Political and Policy Sciences. He is majoring in Economics and Finance, and is a member of Sigma Alpha Epsilon, an Orientation Team Mentor and an employee at the University of Texas at Dallas's Activity Center. Shawn is running for Senate in order to help influence the growth and expansion of the university to the best of his ability for the student body as a whole."
+    },
+    {
+        "name":  "Scott Jones",
+        "pos":   "Junior Senator",
+        "dept":  "Engineering and Computer Science",
+        "img":   "jones.jpg",
+        "email": "Scott.Jones1@utdallas.edu",
+        "bio":   "Scott Jones is running for a junior senator position representing Engineering and Computer Science. He is majoring in Mechanical Engineering, and is involved in Chi Phi, Fellowship of Christian University Students, and the Terry Scholars Organization. Scott is running for Senate because he wants UT Dallas to retain its smaller university feel while expanding greatly."
+    },
+    {
+        "name":  "Nikki Kanthety",
+        "pos":   "Junior Senator",
+        "dept":  "School of Natural Science and Mathematics",
+        "img":   "kanthety.jpg",
+        "email": "nnk09020@utdallas.edu",
+        "bio":   "Nikki Kanthety is running for a junior senator position representing School of Natural Science and Mathematics. She is majoring in Biology, and is involved in Student Government, Auxiliary Services Committee, Dean's Advisory Council, Undergraduate Dean's Advisory Council, been Tournaments and Games Committee Chair of SUAAB, SEBI, UNICEF, Molding Doctors, and Learn to Be. Nikki Kanthety is running for Senate in order to/because I want to help better serve the growing needs of this expanding university and help voice student's opinions on how the campus is shaped in the coming years. Nikki Kanthety's previous senate experience includes serving on the Communications Committee and other various projects done by other committees."
+    },
+    {
+        "name":  "Natasha Upadhyay",
+        "pos":   "Senior Senator",
+        "dept":  "School of Management",
+        "img":   "upadhyay.jpg",
+        "email": "nxu100020@utdallas.edu",
+        "bio":   "Natasha Upadhyay is running for a senior position representing the Naveen Jindal School of Management. She is majoring in Global Business and Finance and is involved with Global Village, International Management Society and the Dean's Council. Natasha Upadhyay is running for Senate to make a change and be the voice of those students with a cause. She is currently serving as a Junior Senator on the Graduate and International Affairs Committee."
+    },
+    {
+        "name":  "Gabrielle Ricks",
+        "pos":   "Junior Senator",
+        "dept":  "School of Economic, Political, and Policy Sciences",
+        "img":   "ricks.jpg",
+        "email": "gbr090020@utdallas.edu",
+        "bio":   "Gabrielle Ricks is running for a junior senator position representing the school of economic policy and political science. She is majoring in political science and is a peer advisor in residence hall north. Gabby is running for senate to make a difference at UTD and to represent the concerns and interests of students at UTD. She has had experience in residential senate freshmen year serving as a liaison for RHA."
+    },
+    {
+        "name":  "Elizabeth Sohns",
+        "pos":   "Junior Senator",
+        "dept":  "School of Management",
+        "img":   "sohns.jpg",
+        "email": "eas105020@utdallas.edu",
+        "bio":   "Elizabeth Sohns is running for a junior senator position representing the School of Management. She is majoring in Global Business and minoring in Political Science. Elizabeth is a member of the NCAA Women's Soccer team, the Pre-Law Society, FOCUS, Delta Epsilon Iota Society, and the CV Honors Program. She is also a PLTL Leader for Calculus. Elizabeth is running for Student Government to help UT Dallas continue to grow to its full potential and establish lasting traditions. She is currently serving as a Sophomore Senator on the Communications Committee, and has also had experience with the Activity Center Committee as well as the UT Dallas Ring Committee."
+    },
+    {
+        "name":  "Alejandro Chinchilla",
+        "pos":   "Junior Senator",
+        "dept":  "Arts and Humanities",
+        "img":   "chinchilla.jpg",
+        "email": "ajc090220@utdallas.edu",
+        "bio":   "Alejandro Chinchilla is running for a (junior senator position representing Arts and Humanities). He is majoring in History and is involved in UTD Rugby, Sigma Alpha Epsilon, First Year Leader, and Orientation Team Mentor, Alejandro Chinchilla is running for Senate in order to help accommodate students needs. Alejandro Chinchilla's previous senate experience includes 1 full term his Sophomore year."
+    },
+    {
+        "name":  "Max Grunewald",
+        "pos":   "Sophomore Senator",
+        "dept":  "Undeclared",
+        "img":   ".jpg",
+        "email": "max.grunewald@utdallas.edu",
+        "bio":   "Max Grunewald is running for a sophomore senate position representing undeclared students. He is interested in studying energy infrastructure and development in Southeast Asia. Currently, he is involved in Pi Kappa Phi as a founding father and a member of its executive board, the UTD debate team, and Students for Environmental Awareness. He is running for a senate seat to appease the political aspirations of his former life in which he wore a large top hat, expanded railway throughout the East and West, and ended slavery between the North and the South."
+    },
+    {
+        "name":  "Liza Liberman",
+        "pos":   "Sophomore Senator",
+        "dept":  "EPPS",
+        "img":   "liberman.jpg",
+        "email": "exl110330@utdallas.edu",
+        "bio":   "Liza Liberman is running for a sophomore senator position representing EPPS. She is majoring in political science, and is involved in Delta Delta Delta sorority, political science club,and the pre-law society. Liza is running for Senate to help insure that the opinions and voices of UTD students are heard. Her previous senate experience includes being a Freshman Senator on the Academic Affairs Committee."
+    },
+    {
+        "name":  "Kaleena Thai",
+        "pos":   "Sophomore Senator",
+        "dept":  "",
+        "img":   ".jpg",
+        "email": "kxt109020@utdallas.edu",
+        "bio":   "Kaleena Thai is running for a sophomore senator position in Student Government. She is majoring in both Biology and Business Administration. She is involved in Alpha Epsilon Delta, The Filipino Student Association, and The Academic Bridge Program. Kaleena is running for senator to ensure that every student has a voice in shaping UT Dallas. She is currently serving as a Freshman Senator on the Student Affairs Committee."
+    }
+]
---
+++ js/load_candidates.js
@@ -0,0 +1,44 @@
+$(function() {
+    // setup a template for each article
+    var article_template = $('<article class="candidate"></article>');
+    var header = $('<header></header>').appendTo(article_template);
+    $(header).append('<h1></h1>');
+    var header_ul = $('<ul class="quick-info"></uL>').appendTo(header);
+    $(header_ul).append("<li><strong>Position: </strong></li>")
+                .append("<li></li>")
+    var content = $('<div class="content"></div>').appendTo(article_template);
+    $(content).append('<div class="img-wrapper"><img src="" alt="Candidate Image" /></div>')
+              .append('<p class="bio"></p>')
+              .append('<p class="contact"></p>');
+
+    $.getJSON('js/ajax/candidates.js', function(candidates) {
+        // candidates = an array of candidate objects
+        for (var i = 0; i < candidates.length; i++) {
+            var c = candidates[i];
+            console.log(c.name);
+            var article = $(article_template).clone( true, true ); // deep clone
+            
+            // setup header
+            if ( c.img != "" && c.img != ".jpg" ) {
+                $(article).find(".content img").attr( 'src',  'images/candidates/' + c.img ); // do img first so it can load; TODO does this actually work like that?
+            } else {
+                $(article).find(".content img").remove();
+            }
+            $(article).find("header h1").text( c.name );
+            $(article).find("header .quick-info > *:nth-child(2)").html( c.pos + "<br />" + c.dept );
+
+            // setup find
+            $(article).find(".content .bio").text( c.bio );
+            $(article).find(".content .contact").html( 'You can contact ' + c.name.split(" ")[0] /* first name only */ + ' at <a href="mailto:' + c.email + '?Subject=SAVE UT Dallas">' + c.email + '</a>.');
+
+            // put in document
+            $("#candidates .content-wrapper").append( article );
+        }
+
+        // odd # of candidates
+        // add in an extra for asthetics
+        if (candidates.length % 2 == 1) {
+            $("#candidates .content-wrapper").append('<article class="candidate"><!--empty for display purposes- odd # candidates otherwise --></article>');
+        }
+    });
+});
